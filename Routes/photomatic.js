eb90d7752321cae28e9ca4ffd5338d1d6d963d49
Justin Loffredo <justloffredo@gmail.com>
2017-06-27 23:47:48 -0400

    basic forms for login and signup added

--- Routes/photomatic.js
+++ Routes/photomatic.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const renderTemplate = require("../utility/renderTemplate.js");
 const BodyParser = require("body-parser");
 const multer = require("multer");
+const User = require("../models/users.js");
 
 
 
@@ -14,9 +15,17 @@ router.get("/", function(req, res) {
 	});
 });
 
+router.get("/login", function(req, res) {
+	renderTemplate(res, "login", "Login", {
 
+	});
+});
 
+router.get("/signup", function(req, res) {
+	renderTemplate(res, "signup", "Signup", {
 
+	});
+});
 
 
 
