19cbad25133a1d5bc442b0d57b31e9aeaca2564e
Justin Loffredo <justloffredo@gmail.com>
2017-07-02 12:44:26 -0400

    initial user authentication commit

--- Routes/photos.js
+++ Routes/photos.js
@@ -1,44 +1,20 @@
-
-require('dotenv').config();
 const express = require ("express");
 const router = express.Router();
 const renderTemplate = require("../utility/renderTemplate.js");
 const BodyParser = require("body-parser");
 const multer = require("multer");
-const User = require("../models/users.js");
-const Sequelize = require("sequelize");
 
 
-router.post("/signup", function(req,res) {
-	User.create({
-		username: req.body.username,
-		password: req.body.password,
-	})
-	.then(function(user) {
-		console.log(user);
-		res.redirect("/");
-	})
-	.catch(function(err) {
-		res.send("Error");
-	});
-});
+
+
+
 
 router.get("/", function(req, res) {
 	renderTemplate(res, "home", "Home", {
 	});
 });
 
-router.get("/login", function(req, res) {
-	renderTemplate(res, "login", "Login", {
 
-	});
-});
-
-router.get("/signup", function(req, res) {
-	renderTemplate(res, "signup", "Signup", {
-
-	});
-});
 
 
 
--- Routes/user.js
+++ Routes/user.js
@@ -3,21 +3,73 @@ const User = require("../models/users.js");
 const renderTemplate = require("../utility/renderTemplate.js");
 const router = express.Router();
 
-router.post("/signup", function(req,res) {
-	
-});
-
 router.get("/login", function(req, res) {
 	renderTemplate(res, "login", "Login", {
-
 	});
 });
 
-router.get("/signup", function(req, res) {
+router.post("/login", function(req, res) {
+	User.findOne({
+		where: {
+			username: req.body.username,
+		},
+	})
+	.then(function(user) {
+		console.log(user.password);
+		console.log(req.body.password);
+
+		if (user) {
+			user.comparePassword(req.body.password)
+				.then(function(valid) {
+					if (valid) {
+						req.session.user = user;
+						res.redirect("/");
+					}
+					else {
+						renderTemplate(res, "login", "Login", {
+							error: "Incorrect password",
+						});
+					}
+				})
+				.catch(function(err) {
+					console.log(err);
+				});
+		}
+		else {
+			renderTemplate(res, "login", "Login", {
+				error: "Username not found",
+			});
+		}
+	})
+	.catch(function(err) {
+			console.log(err);
+			renderTemplate(res, "login", "Login", {
+				error: "Your database has gone to shit",
+			});
+		});
+	});
+
+
+router.get("/signup", function(req, res, error) {
 	renderTemplate(res, "signup", "Signup", {
-		error: error,
-
 	});
 });
+
+
+router.post("/signup", function(req,res) {
+	User.create({
+		username: req.body.username,
+		password: req.body.password,
+	})
+	.then(function() {
+		res.redirect("/user/login");
+	})
+	.catch(function(err) {
+		renderTemplate(req, res, "signup", "Signup", {
+			error: "Please ensure all fields are filled in properly",
+		});
+	});
+});
+
 
 module.exports = router;
