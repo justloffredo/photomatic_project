9c86a56404f543b37327e8e07cb710f7f633909d
David Noriega <dnori817@gmail.com>
2017-07-02 12:51:34 -0400

    basic upload functional

--- Routes/photos.js
+++ Routes/photos.js
@@ -8,6 +8,9 @@ const multer = require("multer");
 const User = require("../models/users.js");
 const Sequelize = require("sequelize");
 
+const Photo = require("../models/photos");
+const uploader = multer({ dest: "uploads/" });
+
 
 router.post("/signup", function(req,res) {
 	User.create({
@@ -40,14 +43,69 @@ router.get("/signup", function(req, res) {
 	});
 });
 
+// upload photo
+// Render an upload form that POSTs to /docs/upload
+router.get("/upload", function(req, res) {
+	// renderTemplate(req, res, "Upload a File", "upload");
+	renderTemplate(res, "upload", "Upload", {
+	});
+});
 
+// Upload the form at GET /docs/upload
+router.post("/upload", uploader.single("file"), function(req, res) {
+	// Make sure they sent a file
+	if (!req.file) {
+		return renderTemplate(req, res, "Upload a File", "upload", {
+			error: "You must choose a file to upload",
+		});
+	}
 
+	// Otherwise, try an upload
+	// req.user.upload(req.file).then(function() {
+	// 	res.redirect("/docs?success=1");
+	// })
+	// .catch(function(err) {
+	// 	console.error("Something went wrong with upload", err);
+	// 	renderTemplate(req, res, "Upload a File", "upload", {
+	// 		error: "Something went wrong, please try a different file",
+	// 	});
+	// });
+});
 
+// Render an individual document
+router.get("/doc/:fileId", function(req, res) {
+	File.findById(req.params.fileId).then(function(file) {
+		if (file) {
+			renderTemplate(req, res, file.get("name"), "document", {
+				file: file,
+			});
+		}
+		else {
+			res.status(404);
+			renderTemplate(req, res, "Not Found", "404");
+		}
+	})
+	.catch(function(err) {
+		console.error("Error while fetching file " + req.params.fileId, err);
+		res.status(500).send("Something went wrong!");
+	});
+});
 
-
-
-
-
+// Download a document, if it exists
+router.get("/download/:fileId", function(req, res) {
+	File.findById(req.params.fileId).then(function(file) {
+		if (file) {
+			res.download("uploads/" + file.get("id"), file.get("originalName"));
+		}
+		else {
+			res.status(404).send("No file found");
+		}
+	})
+	.catch(function(err) {
+		console.error(err);
+		res.status(500).send("Something went wrong");
+	});
+});
 
 
 
